4a11e2fa38157074af9e31c0483850a03b4d7abe
mik5plays <miktheeradon@gmail.com>
2025-05-27 00:15:20 +0700

    Milestone 2

--- icsh.c
+++ icsh.c
@@ -32,24 +32,33 @@ void command_unknown() {
     printf("Bad command.\n");
 }
 
-int main() {
+// Separate function for the shell, as we will use main() to handle arguments
+int shell(FILE *input_stream, int script) {
     char buffer[MAX_CMD_BUFFER];
     char last_command[MAX_CMD_BUFFER] = "";
 
     printf("Starting IC shell...\n"); // Initialization
 
     while (1) {
-        printf("icsh $ ");
+        if (script == 0) { // Only print the prompt if we are not running a script
+            printf("icsh $ ");
+        }
+
+        // Error handling
+        if (fgets(buffer, MAX_CMD_BUFFER, input_stream) == NULL) {
+            break;
+        }
 
         // Handling command inputs, remove trailing newline
-        fgets(buffer, 255, stdin);
         buffer[strcspn(buffer, "\n")] = 0;
 
         // MILESTONE 1: !! (repeat last command)
         // If the last command is bad, it will repeat it anyway.
         if (strcmp(buffer, "!!") == 0) {
             if (last_command[0] == '\0') {
-                printf("No previous command!\n"); // Added notification before giving back prompt
+                if (script == 0) {
+                    printf("No previous command!\n"); // Added notification before giving back prompt
+                }
                 continue;
             }
             strcpy(buffer, last_command); // Save last command as current command
@@ -60,12 +69,29 @@ int main() {
         if (strncmp(buffer, "echo ", 5) == 0) { // echo
             command_echo(buffer);
         } else if (strncmp(buffer, "exit", 4) == 0 && (buffer[4] == ' ' || buffer[4] == '\0')) {
-            int code = command_exit(buffer);
-            return code; // Exit with the specified code
+            return command_exit(buffer);
         } else {
             command_unknown();
         }
     }
 
     return 0;
+}
+int main(int argc, char *argv[]) {
+    // MILESTONE 2: Script Handling
+    if (argc == 2) { // If there are arguments
+        FILE *script = fopen(argv[1], "r");
+
+        // It is imperative we do some error handling first
+        if (!script) {
+            perror("Can't open script, file may not exist/be invalid.");
+            return 1;
+        }
+
+        int code = shell(script, 1); // We are running scripts
+        fclose(script);
+        return code;
+    } else { // We are just running commands
+        return shell(stdin, 0);
+    }
 }
